88dd26fe21675f321b4b2903df280d6e93ea0238
arthur-unitronica <arthur@unitronica.com.br>
2025-06-09 16:32:03 -0300

    should be able to register a new user in the system

---
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Livewire\Auth\Register;
+use Livewire\Livewire;
+
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+
+it('should render the component', function () {
+    Livewire::test(Register::class)
+        ->assertOk();
+});
+
+it('should be able to register a new user in the system', function() {
+    Livewire::test(Register::clas)
+        ->set('name', 'Arthur')
+        ->set('email', 'arthur@email.com')
+        ->set('email_confirmation', 'arthur@email.com')
+        ->set('password', '123')
+        ->call('submit')
+        ->assertHasNoErrors();
+
+    assertDatabaseHas('users', [
+        'name' => 'Arthur',
+        'email' => 'arthur@email.com'
+    ]);
+
+    assertDatabaseCount('users', 1);
+})
